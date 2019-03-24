99df6a062bda5ab4a4983606350b68865f82e89e
Kirill Marchuk <62mkv@mail.ru>
2019-03-24 14:11:52 +0200

    yay!! this works! DLL function is called and correct result is printed

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@
 //procedure sea_liitsqna (i : boolean); far stdcall external 'ana.dll';
 
 extern crate encoding;
-extern crate libloading;
+extern crate libloading as dll;
 
 use std::ffi::CStr;
+use std::io::{Error, ErrorKind};
 
 use encoding::{DecoderTrap, EncoderTrap, Encoding};
 use encoding::all::WINDOWS_1257;
+
+static mut ANA_DLL: Option<dll::Library> = None;
 
 fn analyys(s: &str) -> Result<String, String> {
     match WINDOWS_1257.encode(s, EncoderTrap::Strict) {
@@ -22,14 +25,14 @@ fn analyys(s: &str) -> Result<String, String> {
 }
 
 fn analyze_encoded(mut encoded_word: Vec<u8>) -> Result<String, String> {
-    const LEN: u16 = 4095;
+    const LEN: u16 = 4096;
     encoded_word.resize(4096, 0);
     if let Err(e) = analyze_dll(encoded_word.as_mut_ptr() as *const u8, LEN) {
         return Err(e.to_string());
     }
-    match CStr::from_bytes_with_nul(encoded_word.as_slice()) {
-        Ok(parsed_cstr) => decode_analyze_result(parsed_cstr.to_bytes()),
-        Err(_) => Err(String::from("FromBytesWithNulError"))
+    unsafe {
+        let parsed_cstr = CStr::from_ptr(encoded_word.as_ptr() as *const i8);
+        decode_analyze_result(parsed_cstr.to_bytes())
     }
 }
 
@@ -41,14 +44,24 @@ fn decode_analyze_result(input: &[u8]) -> Result<String, String> {
 }
 
 fn analyze_dll(text: *const u8, len: u16) -> std::io::Result<()> {
-    let lib = libloading::Library::new("ana.dll")?;
     unsafe {
-        let external_analyze: libloading::Symbol<unsafe extern fn(p: *const u8, len: u16) -> ()> = lib.get(b"analyys")?;
-        Ok(external_analyze(text, len))
+        match ANA_DLL {
+            Some(ref lib) => {
+                let external_analyze: libloading::Symbol<unsafe extern "stdcall" fn(p: *const u8, len: u16) -> ()> = lib.get(b"analyys")?;
+                Ok(external_analyze(text, len))
+            }
+            None => Err(Error::from(ErrorKind::NotFound))
+        }
     }
 }
 
 fn main() {
+    unsafe {
+        match dll::Library::new("ana.dll") {
+            Ok(lib) => ANA_DLL = Some(lib),
+            Err(_) => panic!("DLL not loaded")
+        }
+    }
     match analyys("tulla") {
         Result::Ok(s) => println!("Analyys for tulla: {:?}", s),
         Result::Err(e) => println!("Error occurred: {}", e)
