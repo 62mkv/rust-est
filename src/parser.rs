8a703547700471ca795c6c0478626f2eb58b0eee
Kirill Marchuk <62mkv@mail.ru>
2019-08-06 23:16:24 +0300

    WIP: parsing XML and returning Article's (primitive so far, but still)

--- src/parser.rs
+++ src/parser.rs
@@ -1,29 +1,56 @@
-use std::convert::From;
-use std::fmt::Write;
+use std::fmt::{Display, Error, Formatter};
 
 use roxmltree::{Document, Node, NodeType};
 
-fn find_children_by_tagname<'a>(node: &'a Node, path: Vec<&'a str>) -> Result<Vec<'a &Node>, String> {
+fn find_children_by_tagname<'a, 'input: 'a>(node: Node<'a, 'input>, path: Vec<&'input str>) -> Result<Vec<Node<'a, 'input>>, String> {
     if path.len() > 0 {
-        let mut res: Vec<&Node> = Vec::new();
-        for child in node.children().
-            filter(|n| n.tag_name().name().eq(path.get(0))) {
-            if path.len() > 1 {
-                if let Ok(&mut vec) = find_children_by_tagname(&child, path[1..].to_owned()) {
-                    res.append(vec);
+        let mut res: Vec<Node> = Vec::new();
+        for child in node.children()
+            .filter(|n| (&n.tag_name().name()).eq(path.get(0).unwrap()))
+            {
+                if path.len() > 1 {
+                    if let Ok(mut vec) = find_children_by_tagname(child, path[1..].to_owned()) {
+                        res.append(vec.as_mut());
+                    }
+                } else {
+                    res.push(child);
                 }
-            } else {
-                res.push(&child);
             }
-        }
         Ok(res)
     } else {
-        Vec::new();
+        Ok(Vec::new())
     }
 }
 
-fn parse_article(article: &Node) -> Result<String, String> {
-    let mut result = String::new();
+struct Article {
+    lexeme: String,
+    part_of_speech: String,
+}
+
+impl Display for Article {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{} [{}]", self.lexeme, self.part_of_speech)
+    }
+}
+
+fn get_text_from_node(node: Option<&Node>) -> Option<String> {
+    if let Some(n) = node {
+        n.text().map(|s| s.to_string())
+    } else {
+        None
+    }
+}
+
+fn parse_article(article: Node) -> Result<Article, String> {
+    let nodes = find_children_by_tagname(article, vec!["P", "mg"]).unwrap();
+    let node = nodes.get(0).unwrap().to_owned();
+    let body_nodes = find_children_by_tagname(node, vec!["m"]).unwrap();
+    let part_of_speech_nodes = find_children_by_tagname(node, vec!["sl"]).unwrap();
+    let result = Article {
+        lexeme: get_text_from_node(body_nodes.get(0)).unwrap_or("".to_string()),
+        part_of_speech: get_text_from_node(part_of_speech_nodes.get(0)).unwrap_or("".to_string()),
+    };
+    Ok(result)
 }
 
 pub fn parse(input: &str) -> Result<String, String> {
@@ -31,7 +58,7 @@ pub fn parse(input: &str) -> Result<String, String> {
     let mut str = String::new();
     for node in doc.root_element().children()
         .filter(|n| n.node_type() == NodeType::Element) {
-        let mut node_legend = parse_article(&node).unwrap();
+        let node_legend = parse_article(node).unwrap().to_string();
         str.push_str(&node_legend);
         str.push(',');
     }
